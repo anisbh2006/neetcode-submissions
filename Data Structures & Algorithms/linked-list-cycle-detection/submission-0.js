298771098c7e5e8b4e4ebdf8f6f7a23ec1785826
anisbh2006 <106709243+anisbh2006@users.noreply.github.com>
2026-09-24 23:55:04 +0100

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        let slow = head;
+        let fast = head;
+
+        while (fast !== null && fast.next !== null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow === fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
